f036f1fabc0aeab77f086e52f943a1fccd2bec0d
jde <jde@jde.com>
2022-01-14 03:30:42 +0900

    Java Code - baekjoon

--- src/bj1550.java
+++ src/bj1550.java
@@ -2,11 +2,23 @@
  * 16진수 수를 입력받아서 10진수로 출력하는 프로그램을 작성하시오.
  */
 import java.util.Scanner;
+import java.lang.Math;
+
 public class bj1550 {
     public static void main(String [] args){
         int deci = 0;
         Scanner scanner = new Scanner(System.in);
         String hex = scanner.nextLine();
-        System.out.println(hex.length());
+        int length = hex.length();
+
+        int bridge;
+        for(int i = 0; i<length; i++) {
+            bridge = hex.charAt(i);
+            if(47 < bridge && bridge < 58) bridge -= 48;
+            else bridge -= 55;
+            deci += bridge * (int)Math.pow(16, length-i-1);
+        }
+
+        System.out.println(deci);
     }
 }
