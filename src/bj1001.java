5d57a28e4d143e4d38ac5d1667115b08749d83c6
jde <jde@jde.com>
2022-01-14 01:47:40 +0900

    java code

---
+++ src/bj1001.java
@@ -0,0 +1,13 @@
+/*
+ * 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+ */
+import java.util.Scanner;
+public class bj1001 {
+    public static void main(String [] args){
+        int a, b;
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.println(a-b);
+    }
+}
--- src/bj2557.java
+++ src/bj2557.java
@@ -1,2 +1,5 @@
 public class bj2557 {
+    public static void main(String [] args){
+        System.out.println("Hello World!");
+    }
 }
