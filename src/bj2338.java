da7eed278f6c1c73d2b86e58110062b01fcd449b
jde <jde@jde.com>
2022-01-14 14:09:50 +0900

    Java Code

---
+++ src/bj2338.java
@@ -0,0 +1,16 @@
+/*
+ * 두 수 A, B를 입력받아, A+B, A-B, A×B를 구하는 프로그램을 작성하시오.
+ */
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class bj2338 {
+    public static void main(String [] args){
+        Scanner s = new Scanner(System.in);
+        BigInteger a = new BigInteger(s.next());
+        BigInteger b = new BigInteger(s.next());
+        System.out.println(a.add(b));
+        System.out.println(a.subtract(b));
+        System.out.println(a.multiply(b));
+    }
+}
