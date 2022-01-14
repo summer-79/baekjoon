eeff259b091c18f0c493b8a86a69733bdf870dde
jde <jde@jde.com>
2022-01-14 15:20:30 +0900

    Java Code 20220114

--- baekjoon/bj2338.java
+++ baekjoon/bj2338.java
@@ -4,7 +4,7 @@
 import java.util.Scanner;
 import java.math.BigInteger;
 
-public class bj2338 {
+public class Main {
     public static void main(String [] args){
         Scanner s = new Scanner(System.in);
         BigInteger a = new BigInteger(s.next());
